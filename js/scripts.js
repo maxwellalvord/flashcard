45b50b827411ea38789e9ed42c8c650deb93eaea
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-01 15:01:43 -0700

    creation of CS flashcards, practice using bootstrap cards

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $(".walrus-showing1").toggle();
+    $(".walrus-hidden1").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $(".walrus-showing2").toggle();
+    $(".walrus-hidden2").toggle();
+  });
+});
